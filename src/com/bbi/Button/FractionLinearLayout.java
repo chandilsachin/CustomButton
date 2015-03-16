a372aefb2c5fbcf9ffd2affdf75b5134461e9ef3
Sachin Chandil <sachin.chandil@media4u.com>
2015-03-16 19:09:11 +0530

    Modified

---
+++ src/com/bbi/Button/FractionLinearLayout.java
@@ -0,0 +1,46 @@
+package com.bbi.Button;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class FractionLinearLayout extends LinearLayout {
+
+	public FractionLinearLayout(Context context, AttributeSet attrs,
+			int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FractionLinearLayout(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public FractionLinearLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public float getFractionTranslationX() {
+		
+		return getWidth() > 0 ? super.getTranslationX() / getWidth() : Float.MAX_VALUE;
+	}
+	
+	public void setFractionTranslationX(float translationX) {
+		
+		int width = getWidth();
+		super.setTranslationX(width > 0 ? width * translationX : Float.MAX_VALUE);
+	}
+	
+	public float getFractionTranslationY() {
+		
+		return getHeight() > 0 ? super.getTranslationX() / getHeight() : Float.MAX_VALUE;
+	}
+	
+	public void setFractionTranslationY(float translationY) {
+		int height = getHeight();
+		super.setTranslationY(height > 0 ? height * translationY : Float.MAX_VALUE);
+	}
+
+}
--- src/com/example/custombutton/MainActivity.java
+++ src/com/example/custombutton/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.custombutton;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.bbi.Button.OverlayNotificationView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +17,28 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button btn = (Button) findViewById(R.id.btn1);
+		
+		
+		final OverlayNotificationView view = (OverlayNotificationView) findViewById(R.id.overlayView);
+		//view.setTitle("Title");
+		view.setTitleColor(Color.MAGENTA);
+		view.setType(OverlayNotificationView.IMAGE);
+		view.setContentImageResource(R.drawable.ic_launcher);
+		view.setFractionalHeight(50);
+		view.setSliderImageResource(R.drawable.tab_frwrd_grey);
+		//view.setAnimationType(OverlayNotificationView.ANIMATION_RIGHT_TO_LEFT);
+		view.setAnimationType(OverlayNotificationView.ANIMATION_LEFT_TO_RIGHT);
+		view.setManualOpen(false);
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				
+				view.triggerState();
+			}
+		});
 	}
 
 	@Override
